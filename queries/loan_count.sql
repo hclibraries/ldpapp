4a58905c0494df46742fa6e8d8d895f8aa85d17c
hclibraries <113636312+hclibraries@users.noreply.github.com>
2025-02-10 17:24:51 -0500

    Update loan_count.sql

--- queries/loan_count.sql
+++ queries/loan_count.sql
@@ -1,4 +1,8 @@
 --metadb:function loan_count
+CREATE OR REPLACE FUNCTION count_loans(start_date date DEFAULT '2000-01-01'::date, end_date date DEFAULT '2050-01-01'::date)
+ RETURNS TABLE(item_id uuid, loan_count integer)
+ LANGUAGE sql
+AS $function$
 SELECT 
     item_id,
     COUNT(*) AS loan_count
@@ -6,3 +10,5 @@ FROM folio_circulation.loan__t
 WHERE start_date <= loan_date::DATE  -- Explicitly cast loan_date to DATE
 AND loan_date::DATE < end_date
 GROUP BY item_id;
+$function$
+;
