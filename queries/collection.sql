a1525a09400f93be3a16b3d1a685a59dccff21d1
hclibraries <113636312+hclibraries@users.noreply.github.com>
2025-02-10 14:36:23 -0500

    Delete queries directory

--- queries/collection.sql
+++
@@ -1,4 +0,0 @@
-SELECT round(
-           ( (SELECT count(DISTINCT item_id)::float FROM folio_circulation.loan__t) /
-             (SELECT count(*)::float FROM folio_inventory.item__t) )::numeric,
-           2) AS loan_quotient;
