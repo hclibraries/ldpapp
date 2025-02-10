6b5b8809775c872fc9d9a0b201013ff67b139be0
hclibraries <113636312+hclibraries@users.noreply.github.com>
2025-02-10 13:11:52 -0500

    Update collection-use.sql

--- queries/collection-use.sql
+++ queries/collection-use.sql
@@ -1,4 +1,1 @@
-SELECT round(
-           ( (SELECT count(DISTINCT item_id)::float FROM folio_circulation.loan__t) /
-             (SELECT count(*)::float FROM folio_inventory.item__t) )::numeric,
-           2) AS loan_quotient;
+SELECT 1;
