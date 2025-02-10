4377a3ebe4b1e271e3c43b2a250c12952f8ff812
hclibraries <113636312+hclibraries@users.noreply.github.com>
2025-02-10 14:05:13 -0500

    Update users.sql

--- queries/users.sql
+++ queries/users.sql
@@ -1,16 +1,4 @@
-CREATE FUNCTION get_users(
-    start_date date DEFAULT '2000-01-01',
-    end_date date DEFAULT '2050-01-01'
-) 
-RETURNS TABLE(
-    id text, 
-    barcode text, 
-    created_date timestamp with time zone
-) 
-AS $$ 
-    SELECT id::text, barcode, created_date 
-    FROM user_users 
-    WHERE user_users.created_date >= start_date 
-    AND user_users.created_date <= end_date 
-$$ 
-LANGUAGE SQL STABLE PARALLEL SAFE;
+SELECT round(
+           ( (SELECT count(DISTINCT item_id)::float FROM folio_circulation.loan__t) /
+             (SELECT count(*)::float FROM folio_inventory.item__t) )::numeric,
+           2) AS loan_quotient;
