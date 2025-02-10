ad4dc0a2001308e05201ef3988348102792bf003
hclibraries <113636312+hclibraries@users.noreply.github.com>
2025-02-10 13:27:13 -0500

    Update test.sql

--- queries/test.sql
+++ queries/test.sql
@@ -1,19 +1,19 @@
-{
-    "displayName": "List users by date of creation",
-    "description": "Created by Kurt to exercise mod-ldp's reporting facility",
-    "instructions": "Choose a start and end date for the user-creation period.",
-    "parameters": [
-        {
-            "name": "start_date",
-            "displayName": "Earliest date of user creation",
-            "type": "date",
-            "required": false
-        },
-        {
-            "name": "end_date",
-            "displayName": "Latest date of user creation",
-            "type": "date",
-            "required": false
-        }
-    ]
-}
+--ldp:function get_users
+
+CREATE FUNCTION get_users(
+    start_date date DEFAULT '2000-01-01',
+    end_date date DEFAULT '2050-01-01'
+)
+RETURNS TABLE(
+    id text,
+    barcode text,
+    created_date timestamp with time zone)
+AS $$
+SELECT id::text, barcode, created_date
+
+    FROM user_users
+    WHERE user_users.created_date >= start_date AND user_users.created_date <= end_date
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
