7f55891a3489f80c7be6139ba813181f5d9a4b6a
hclibraries <113636312+hclibraries@users.noreply.github.com>
2025-02-12 12:29:17 -0500

    Update missing-items.sql

--- queries/ldp/missing-items.sql
+++ queries/ldp/missing-items.sql
@@ -1,1 +1,15 @@
-test
+SELECT
+	ihi.title,
+	ie.barcode,
+	ie.effective_location_name,
+	ie.effective_call_number,
+	ie.volume,
+	ie.enumeration,
+	ie.item_hrid,
+	status__name,
+	status__date,
+	ie.updated_date
+FROM public.inventory_items AS ii
+LEFT JOIN folio_reporting.items_holdings_instances AS ihi ON ii.id = ihi.item_id
+LEFT JOIN folio_reporting.item_ext AS ie ON ii.id = ie.item_id
+WHERE Status__name ~ '.*issing.*' OR Status__name ~ '.*ost.*'
